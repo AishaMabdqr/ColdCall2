180476bbd36c1fdf788a17f09bc7fb0a7962c314
Aisha Abdq <Ashmq22@gmail.com>
2021-12-09 17:43:12 +0300

    Initial Commit

---
+++ ColdCall2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColdCall2
+//
+//  Created by A Ab. on 05/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
