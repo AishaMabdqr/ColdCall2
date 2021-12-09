3ecac0acf83a8363c64257af96d3a70d21892b02
Aisha Abdq <Ashmq22@gmail.com>
2021-12-09 18:20:24 +0300

    Cold Call

--- ColdCall2/ViewController.swift
+++ ColdCall2/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var number: UILabel!
+    
+    let names = ["Aisha", "Hessah", "Abdullah", "Ahmad", "Abdulrahman", "Sarah"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        name.text = "Ready?"
+        number.isHidden = true
     }
 
-
+   
+    @IBAction func chooseRandom(_ sender: UIButton) {
+        let randomName = Int.random(in: 0...4)
+        name.text = names[randomName]
+        
+        let randomNum = Int.random(in: 1...5)
+        if (randomNum == 1 || randomNum == 2){
+            number.text = String(randomNum)
+            number.textColor = .red
+            
+            
+        }
+        else if (randomNum == 3 || randomNum == 4){
+            number.text = String(randomNum)
+            number.textColor = .orange
+            
+        }
+        else if (randomNum == 5 ){
+            number.text = String(randomNum)
+            number.textColor = .green
+            
+        }
+        number.isHidden = false
+    }
+    
 }
 
